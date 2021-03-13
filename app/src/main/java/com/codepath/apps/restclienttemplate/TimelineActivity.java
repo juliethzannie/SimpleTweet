214314a1cb10c72aa1beb5d4ddec6f8e13bdf4ea
juliethzannie <jzannie2@u.rochester.edu>
2021-03-12 20:43:12 -0500

    Second Part

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,19 +1,25 @@
 package com.codepath.apps.restclienttemplate;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +28,7 @@ import okhttp3.Headers;
 
 public class TimelineActivity extends AppCompatActivity {
     public static final String TAG = "TimelineActivity";
+    private final int REQUEST_CODE = 20;
     TwitterClient client;
     RecyclerView rvTweets;
     List<Tweet> tweets;
@@ -63,35 +70,74 @@ public class TimelineActivity extends AppCompatActivity {
         scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
             @Override
             public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
-                 Log.i(TAG,"onLoadMore: " + page);
+                Log.i(TAG, "onLoadMore: " + page);
                 loadMoreData();
             }
         };
-         rvTweets.addOnScrollListener(scrollListener);
+        rvTweets.addOnScrollListener(scrollListener);
         populateHomeTimeline();
     }
 
     private void loadMoreData() {
-      client.getNextPageOfTweets(new JsonHttpResponseHandler() {
-          @Override
-          public void onSuccess(int statusCode, Headers headers, JSON json) {
-              Log.i(TAG, "OnSuccess for loadMoreData"+json.toString());
-              JSONArray jsonArray = json.jsonArray;
-              try {
-                  List<Tweet> tweets = Tweet.fromJsonArray(jsonArray);
-                  adapter.addAll(tweets);
+        client.getNextPageOfTweets(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.i(TAG, "OnSuccess for loadMoreData" + json.toString());
+                JSONArray jsonArray = json.jsonArray;
+                try {
+                    List<Tweet> tweets = Tweet.fromJsonArray(jsonArray);
+                    adapter.addAll(tweets);
 
-              } catch (JSONException e) {
-                  e.printStackTrace();
-              }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
-          }
+            }
 
-          @Override
-          public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-              Log.e(TAG, "OnFailure for loadMoreData",throwable);
-          }
-      }, tweets.get(tweets.size()-1).id);
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.e(TAG, "OnFailure for loadMoreData", throwable);
+            }
+        }, tweets.get(tweets.size() - 1).id);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.compose){
+            Toast.makeText(this,"Compose!", Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(this,ComposeActivity.class);
+            startActivityForResult(intent, REQUEST_CODE);
+
+
+            return true;
+        }
+            return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if(requestCode == REQUEST_CODE && requestCode == RESULT_OK ){
+             Tweet tweet = Parcels.unwrap(data.getParcelableExtra("tweet"));
+
+             tweets.add(0,tweet);
+             adapter.notifyItemInserted(0);
+             rvTweets.smoothScrollToPosition(0);
+             //rvTweets.getLayoutManager().scrollToPosition(0);
+             //rvTweets.scrollToPosition(0);
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     private void populateHomeTimeline() {
